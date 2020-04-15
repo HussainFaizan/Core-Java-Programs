9e27bd727091641287d280e9cf9f015feb571451
HussainFaizan <hussainfaizan313@gmail.com>
2020-04-15 15:30:16 +0530

    Added solp;

--- Faizan.java
+++ Faizan.java
@@ -3,4 +3,6 @@ public class Faizan {
     public static void main(String[] args) {
         System.out.println("Hello Faizan...");
         System.out.println("Hi");
+        System.out.println("Hello Java Peoples");
     }
+}
