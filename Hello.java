879c9fc11bfff629631484f3fb0cc075de0a8afb
HussainFaizan <hussainfaizan313@gmail.com>
2020-04-20 02:55:23 -0700

    Third Commit

--- Hello.java
+++ Hello.java
@@ -1,7 +1,6 @@
 class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hi This Faizan");
-		System.out.println("Java Developer");
-		System.out.println("Hello world...");
+
 	}
 }
