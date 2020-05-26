90f69c85e3147a8daf23dc3af5d4da07661e3d09
HussainFaizan <hussainfaizan313@gmail.com>
2020-05-26 18:58:32 +0530

    26 may 2020

---
+++ Array/ArrayDemo.java
@@ -0,0 +1,14 @@
+public class ArrayDemo {
+    public static void main(String[] args) {
+        int arr[] = new int[5];
+        arr[0] = 11;
+        arr[1] = 22;
+        arr[2] = 33;
+        arr[3] = 44;
+        arr[4] = 55;
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("Array list::" + arr[i]);
+        }
+
+    }
+}
